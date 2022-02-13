18cf0f4107940496fb06f17443701e7af176280e
kulchaka <kulchaka@gmail.com>
2022-02-13 20:24:32 +0100

    add project

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+import data from "./data/export_full.xml";
+import XMLParser from "react-xml-parser/xmlParser";
+import Preloader from "./components/Preloader";
+
+function App() {
+  const [state, setState] = useState([]);
+  const [loader, setLoader] = useState(false)
+
+  useEffect(
+    () => axios
+      .get(data, {"Content-Type": "application/xml; charset=utf-8"})
+      .then((res) => {
+        const jsonDataFromXml = new XMLParser().parseFromString(res.data);
+        setState(jsonDataFromXml.getElementsByTagName('item'));
+        setLoader(true)
+      }),
+    []
+  );
+
+  return (
+    <div className='App'>
+      <h1>Total: {state.length}</h1>
+      {
+        loader
+          ?
+          (
+            <ul className="collection with-header">
+              {state.slice(0, 50).map(e => {
+                return <li className="collection-item" key={e.attributes.code}>
+                  <div>{e.attributes.name}
+                    <div href="#!" className="secondary-content">
+                      <div>{e.attributes.czk} CZK</div>
+                    </div>
+                  </div>
+                </li>
+              })}
+            </ul>
+          )
+          :
+          (<Preloader/>)
+      }
+    </div>
+  );
+}
+
+export default App;
